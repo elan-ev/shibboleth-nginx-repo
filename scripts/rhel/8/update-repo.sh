480366f1cf2abb834e1a7266fcc04dceb36f3d2e
Waldemar Smirnow <wsmirnow@uni-osnabrueck.de>
2023-12-01 13:21:48 +0100

    GitHub actions workflow dispatch implemented for EL8

--- scripts/rhel/8/update-repo.sh
+++ scripts/rhel/8/update-repo.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
+# If true, the RPM repository will be recreated.
+# If false or unset, only new packages will be added to the repository.
+_FORCE_RECREATE_REPO="${FORCE_RECREATE_REPO:-false}"
+
 dnf install -y createrepo
 
 pushd /repo 2>/dev/null
-createrepo --update .
+if [ "true" == "$_FORCE_RECREATE_REPO" ]
+then
+    echo "Recreate RPM repository"
+    createrepo .
+else
+    echo "Update RPM repository"
+    createrepo --update .
+fi
 popd 2>/dev/null
