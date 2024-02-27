1ad185dac2b38736044469f8900d3ca701b71e36
Waldemar Smirnow <wsmirnow@uni-osnabrueck.de>
2024-02-27 18:38:08 +0100

    added copyright to debian packages

--- scripts/debian/bookworm/build-shib-nginx-mod.sh
+++ scripts/debian/bookworm/build-shib-nginx-mod.sh
@@ -26,6 +26,42 @@ mkdir -p "$PKG_ROOT/usr/lib/nginx/modules" "$PKG_ROOT/usr/share/nginx/modules-av
 install -o root -g root -m 0644 -p nginx-"$NGINX_VERSION"/objs/ngx_http_shibboleth_module.so "$PKG_ROOT/usr/lib/nginx/modules/"
 echo "load_module modules/ngx_http_shibboleth_module.so;" > mod-http-shibboleth.conf
 install -o root -g root -m 0644 -p mod-http-shibboleth.conf "$PKG_ROOT/usr/share/nginx/modules-available/"
+mkdir -p "$PKG_ROOT/usr/share/doc/libnginx-mod-http-shibboleth/"
+cat <<-EOF "$PKG_ROOT/usr/share/doc/libnginx-mod-http-shibboleth/copyright"
+Format: https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
+Upstream-Name: nginx-http-shibboleth
+Upstream-Contact: David Beitey
+ Luca Bruno
+Source: https://github.com/nginx-shib/nginx-http-shibboleth
+
+Files: *
+Copyright: 2013-present, David Beitey (davidjb)
+ 2014, Luca Bruno
+License: All rights reserved
+
+License: All rights reserved
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+THIS SOFTWARE IS PROVIDED BY AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGE.
+EOF
+chmod 0644 "$PKG_ROOT/usr/share/doc/libnginx-mod-http-shibboleth/copyright"
 mkdir -p "$PKG_ROOT/DEBIAN"
 cat <<EOF > "$PKG_ROOT/DEBIAN/control"
 Package: libnginx-mod-http-shibboleth
