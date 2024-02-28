6d98617a3e514ece0b681989c9f1103dd8376f52
Waldemar Smirnow <wsmirnow@uni-osnabrueck.de>
2024-02-28 11:07:48 +0100

    ubuntu focal build created

---
+++ scripts/ubuntu/focal/update-repo.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env sh
+
+_PWD="$PWD"
+set -e
+apt update
+apt install -y dpkg-dev
+mkdir -p /repo/dists/focal/multiverse/binary-amd64
+cd /repo/
+dpkg-scanpackages --arch amd64 --multiversion pool/ > dists/focal/multiverse/binary-amd64/Packages
+gzip -9 > dists/focal/multiverse/binary-amd64/Packages.gz < dists/focal/multiverse/binary-amd64/Packages
+
+hash_files() {
+    FILES_TO_HASH="$*"
+    echo MD5Sum:
+    for FILE_TO_HASH in $FILES_TO_HASH; do
+        case $FILE_TO_HASH in
+        Release*);;
+        *) echo " $(md5sum $FILE_TO_HASH | cut -d' ' -f1) $(wc -c $FILE_TO_HASH)";;
+        esac
+    done
+    echo SHA1:
+    for FILE_TO_HASH in $FILES_TO_HASH; do
+        case $FILE_TO_HASH in
+        Release*);;
+        *) echo " $(sha1sum $FILE_TO_HASH | cut -d' ' -f1) $(wc -c $FILE_TO_HASH)";;
+        esac
+    done
+    echo SHA256:
+    for FILE_TO_HASH in $FILES_TO_HASH; do
+        case $FILE_TO_HASH in
+        Release*);;
+        *) echo " $(sha256sum $FILE_TO_HASH | cut -d' ' -f1) $(wc -c $FILE_TO_HASH)";;
+        esac
+    done
+}
+
+cd dists/focal
+cat <<EOF > Release
+Origin: Shibboleth Nginx Module Repository
+Suite: stable
+Codename: focal
+Version: 1.0
+Architectures: amd64
+Components: multiverse
+Description: This repository provides a build of Shibboleth auth request module for Nginx.
+Date: $(date -Ru)
+$(hash_files multiverse/binary-amd64/Packages*)
+EOF
+# Sign repo
+apt install gpg
+echo -n "$GPG_SIGNING_KEY" | base64 --decode | gpg --import
+cat Release | gpg -abs > Release.gpg
+cat Release | gpg -abs --clearsign > InRelease
+cd "$_PWD"
